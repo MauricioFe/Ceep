41eeeffbe2b2d57bbc0a805e8945450bd5bcf21b
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-31 21:34:05 -0300

    Criando layouts para implementação da lista

--- app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/com/example/ceep/ui/activity/ListaNotasActivity.java
@@ -1,14 +1,16 @@
-package com.example.ceep;
+package com.example.ceep.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ceep.R;
+
+public class ListaNotasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_notas);
     }
 }
